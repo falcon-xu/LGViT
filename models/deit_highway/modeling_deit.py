36dd7a19a290df5cad41a60dafae8ab39a893885
Javen Howard <63662290+wei-seven@users.noreply.github.com>
2024-10-29 20:07:37 +0800

    Update modeling_deit.py

--- models/deit_highway/modeling_deit.py
+++ models/deit_highway/modeling_deit.py
@@ -70,12 +70,23 @@ class DeiTEmbeddings(nn.Module):
     def __init__(self, config: DeiTConfig, use_mask_token: bool = False) -> None:
         super().__init__()
 
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size))
-        self.distillation_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size))
+        self.config = config
+        
         self.mask_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size)) if use_mask_token else None
         self.patch_embeddings = DeiTPatchEmbeddings(config)
         num_patches = self.patch_embeddings.num_patches
-        self.position_embeddings = nn.Parameter(torch.zeros(1, num_patches + 2, config.hidden_size))
+        
+        if config.backbone == 'DeiT':
+            self.cls_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size))
+            self.distillation_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size))
+            self.position_embeddings = nn.Parameter(torch.zeros(1, num_patches + 2, config.hidden_size))
+        elif config.backbone == 'ViT':
+            self.cls_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size))
+            self.position_embeddings = nn.Parameter(torch.zeros(1, num_patches + 1, config.hidden_size))
+                
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size)) if use_mask_token else None
+        self.patch_embeddings = DeiTPatchEmbeddings(config)
+        num_patches = self.patch_embeddings.num_patches
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
     def forward(self, pixel_values: torch.Tensor, bool_masked_pos: Optional[torch.BoolTensor] = None) -> torch.Tensor:
@@ -88,9 +99,14 @@ class DeiTEmbeddings(nn.Module):
             mask = bool_masked_pos.unsqueeze(-1).type_as(mask_tokens)
             embeddings = embeddings * (1.0 - mask) + mask_tokens * mask
 
-        cls_tokens = self.cls_token.expand(batch_size, -1, -1)
-        distillation_tokens = self.distillation_token.expand(batch_size, -1, -1)
-        embeddings = torch.cat((cls_tokens, distillation_tokens, embeddings), dim=1)
+        if self.config.backbone == 'DeiT':
+            cls_tokens = self.cls_token.expand(batch_size, -1, -1)
+            distillation_tokens = self.distillation_token.expand(batch_size, -1, -1)
+            embeddings = torch.cat((cls_tokens, distillation_tokens, embeddings), dim=1)
+        elif self.config.backbone == 'ViT':
+            cls_tokens = self.cls_token.expand(batch_size, -1, -1)
+            embeddings = torch.cat((cls_tokens, embeddings), dim=1)
+            
         embeddings = embeddings + self.position_embeddings
         embeddings = self.dropout(embeddings)
         return embeddings
